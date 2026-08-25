39f14a9934358413a9c22b3943dc65f431e3b3f4
arunkumar <allamarunkumar@gmail.com>
2026-08-25 20:02:58 +0530

    Tesss

---
+++ Polymorphism/Class6.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Tesla
+    {
+        public virtual void Show()
+        {
+            Console.WriteLine("Hi Iam Show Method from Tesla class");
+        }
+        public void Dispaly()
+        {
+            Console.WriteLine("Hi Iam Display Method from Tesla class");
+        }
+    }
+
+    class Tech : Tesla
+    {
+        public void Hi()
+        {
+            Console.WriteLine("Hi Iam Hi metho from Tech class");
+        }
+        public void Bye()
+        {
+            Console.WriteLine("Hi I am Bye Method From Tech class");
+        }
+        public override void Show()
+        {
+            Console.WriteLine("Hi iam Show Method from Tech class");
+        }
+    }
+    internal class Class6
+    {
+        static void Main(string[] args)
+        {
+            Tech t = new Tech();
+            t.Dispaly();
+            t.ToString();
+        }
+    }
+}
---
+++ Polymorphism/Class7.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace Polymorphism
+{
+    class Student
+    {
+        public int StudentId { get; private set; }
+        public string StudentName { get; private set; }
+        public Student(int StudentId, string StudentName)
+        {
+         this.StudentId = StudentId;
+         this.StudentName= StudentName;
+        }
+        public override string ToString()
+        {
+            return $"Student ID : {StudentId}\tStudent Name : {StudentName}";
+        }
+    }
+    internal class Class7
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student(111,"abc");
+            Console.WriteLine(s1);
+           
+           
+        }
+    }
+}
