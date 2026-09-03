8d3bdcf35eab3a2c851e8f7c5b2d8cb682d11019
arunkumar <allamarunkumar@gmail.com>
2026-09-03 20:07:29 +0530

    Collections

---
+++ CollectionFramework/Class1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionFramework
+{
+
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            Stack st = new Stack();
+            object ob;
+        Menu:
+            Console.Write("1.Add\n2.Delete\n3.Disply\n4.Check\n5.Top\n6.Elements Count\nEnter Your choice : ");
+            int choice = int.Parse(Console.ReadLine());
+            Console.Clear();
+            
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Enter an Element to Add : ");
+                    ob = Console.ReadLine();
+                    st.Push(ob);
+                    goto Menu;
+                case 2:
+                    if(st.Count== 0)
+                    {
+                        Console.WriteLine($"No Elements to delete");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Deleted Element is : {st.Pop()}");
+                    }
+                    goto Menu;
+                case 3:
+                    if (st.Count == 0)
+                    {
+                        Console.WriteLine("No Elements Found");
+                    }
+                    else
+                    {
+                        foreach (var item in st)
+                        {
+                            Console.WriteLine(item);
+                        }
+                    }
+                    goto Menu;
+                case 4:
+                    Console.Write("Enter element : ");
+                     ob = Console.ReadLine();
+                    if (st.Contains(ob))
+                    {
+                        Console.WriteLine($"{ob} Element Exists");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{ob} Element Not Exists");
+                    }
+                    goto Menu;
+                case 5:
+                    if (st.Count == 0)
+                    {
+                        Console.WriteLine($"No Elements to display");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Top Element in the Stack is : {st.Peek()}");
+                    }
+                        goto Menu;
+                case 6:
+                    Console.WriteLine($"Total Elements in the Stack are :{st.Count}");
+                    goto Menu;
+                default:
+                    Console.WriteLine("Invalid choice...");
+                    goto Menu;
+            }
+        }
+    }
+}
---
+++ CollectionFramework/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+
+
+namespace CollectionFramework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        
+
+            Stack s = new Stack();  // Last In First Out
+            s.Push(18);
+            s.Push("Lakshmi");
+            s.Push(6.7);
+            s.Push(true);
+            s.Push('+');
+            Console.WriteLine($"No Of elements present in the stack is :{s.Count}");
+            object r = s.Clone();
+            foreach(object ob in r as Stack)
+            {
+                Console.WriteLine(ob);
+            }
+            Console.WriteLine("************************");
+            foreach(object ob in s)
+            {
+                Console.WriteLine(ob);
+            }
+           
+            Console.WriteLine($"Top or Last Inserted Element in the Stack : {s.Peek()}");
+            Console.WriteLine($"Deleted Element is : {s.Pop()}");
+            Console.WriteLine("==============Elements After deleting the element from the Stack are====");
+            foreach (object ob in s)
+            {
+                Console.WriteLine(ob);
+            }
+
+           
+
+        }
+    }
+}
